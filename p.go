8ac6a254ae1cf09000db1b11cb5ad4e26b997a4e
Martin Durhuus <martindurhuus@gmail.com>
2022-06-26 15:11:46 +0200

    Adds new and open commands. Updates reading config for custom open commands (currently not processing path)

--- p.go
+++ p.go
@@ -6,24 +6,69 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
 type P struct {
 	projectsDir string
+    openCmd string
 }
 
-func (p *P) ls() {
-	files, err := ioutil.ReadDir(p.projectsDir)
+func getProjects(rootDir string) []string {
+	files, err := ioutil.ReadDir(rootDir)
+    var projects []string
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, file := range files {
 		if file.IsDir() {
-			fmt.Println(file.Name())
+            projects = append(projects, file.Name())
 		}
 	}
+
+    return projects
+}
+
+func (p *P) ls() {
+    projects := getProjects(p.projectsDir)
+
+    for _, projectName := range projects {
+        fmt.Println(projectName)
+    }
+}
+
+func (p *P) new(name string) {
+    path := filepath.Join(p.projectsDir, name)
+    if err := os.Mkdir(path, os.ModePerm); err != nil {
+        fmt.Printf("Could not create project directory! %v", err)
+    }
+}
+
+func (p *P) open(name string) {
+    project := filepath.Join(p.projectsDir, name)
+    err := os.Chdir(project)
+
+    if err != nil {
+        fmt.Printf("Could not change to project directory. Error: %v", err)
+    }
+
+    cmdToRun := "vim"
+    if p.openCmd != "" {
+        cmdToRun = p.openCmd
+    }
+
+    cmd := exec.Command(cmdToRun)
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+
+    err = cmd.Run()
+    if err != nil {
+        fmt.Println(err)
+    }
 }
 
 func readConfig(p *P) {
@@ -39,11 +84,23 @@ func readConfig(p *P) {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		if scanner.Text()[0:1] != "#" { // Ignore comments
+		if len(scanner.Text()) == 0 || scanner.Text()[0:1] != "#" { // Ignore comments
 			conf := strings.Split(scanner.Text(), "=")
 			if conf[0] == "projects" {
 				p.projectsDir = conf[1]
-			}
+			} else if conf[0] == "open" {
+
+                if strings.Contains(conf[1], "${projects}") {
+                    // Make sure the projects setting is set, when referenced in 'open' setting.
+                    if p.projectsDir == "" {
+                        fmt.Println("Failed to read config!")
+                        fmt.Println("Make sure the 'projects' setting is set in your p.conf, and that it comes before the 'open' setting")
+                        return
+                    }
+                }
+
+                p.openCmd = conf[1]
+            }
 		}
 	}
 }
@@ -52,10 +109,29 @@ func main() {
 	p := P{}
 	readConfig(&p)
 	numArgs := len(os.Args[1:])
+    args := os.Args[1:]
 
 	if numArgs <= 0 {
 		fmt.Println("Common usage: ls")
-	} else {
-		p.ls()
+        return
 	}
+
+    switch args[0] {
+    case "ls":
+        p.ls()
+    case "new":
+        if numArgs < 2 {
+            fmt.Println("'new' command requires a project name")
+            return
+        }
+        p.new(args[1])
+    case "open":
+        if numArgs < 2 {
+            fmt.Println("'open' command requires a project name")
+            return
+        }
+        p.open(args[1])
+    default:
+        fmt.Printf("'%v' command not supported\n", args[0])
+    }
 }
